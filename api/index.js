0cd557138502658c63f94deeca5af8948f76864b
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-07-28 09:31:21 +0530

    update

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.get("/status", (req, res) => {
   res.send("Hello, World for status!");
 });
 app.get("/status:name", (req, res) => {
-  const { id } = req.params;
+  const { name } = req.params;
 
   res.send(`name, ${name}!`);
 });
