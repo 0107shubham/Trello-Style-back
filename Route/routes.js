7a2f86bd9376366a570a981c767885764d0afbaa
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-07-29 00:02:38 +0530

    update signin

--- Route/routes.js
+++ Route/routes.js
@@ -5,8 +5,11 @@ import { createPost } from "../controller/createPost.js";
 import { deletePost } from "../controller/deletePost.js";
 import { getPost } from "../controller/getPost.js";
 import { updatePost } from "../controller/updatePost.js";
-
+import { signup } from "../controller/signup.js";
+import { signin } from "../controller/signin.js";
 route.post("/user", User);
+route.post("/signup", signup);
+route.post("/signin", signin);
 
 route.post("/post", createPost);
 route.delete("/posts/:id", deletePost);
