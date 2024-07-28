016e2ddb7a8f1c545fe6be36e02d5c8a53e8efe4
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-07-28 09:26:29 +0530

    update

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World for shbham!");
+});
+app.get("/status", (req, res) => {
+  res.send("Hello, World for status!");
 });
 
 app.listen(port, () => {
